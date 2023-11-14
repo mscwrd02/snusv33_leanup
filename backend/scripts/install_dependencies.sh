8417f10e6c33f54c55cbb50d240193f373646a95
mscwrd02 <ehdtodvodl@gmail.com>
2023-11-15 02:54:28 +0900

    please

--- backend/scripts/install_dependencies.sh
+++ backend/scripts/install_dependencies.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/my-nest-app
 
 npm install
 
-aws s3 cp s3://mscwrd02-github-action-s3-bucket/.env /home/ubuntu/my-nest-app/dist
+aws s3 cp s3://mscwrd02-github-action-s3-bucket/.env /home/ubuntu/my-nest-app/
