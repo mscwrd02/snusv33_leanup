b68d6f1eaff1aacbc904356c67f9e2299516058f
Yang1jae <cjuz89@naver.com>
2023-11-18 12:46:42 +0900

    publish

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Page1 from "./Page1";
+import Page1_1 from './Page1_1';
 import Page2_1 from "./Page2_1";
 import Page2_2 from "./Page2_2";
 import Page3 from "./Page3";
-import Page1_1 from './Page1_1';
 
 function App(){
   return(
       <BrowserRouter>
           <Routes>
               <Route path="/">
-                  <Route index element={<Page3 />} />
+                  <Route index element={<Page1 />} />
                   <Route path="page1" element={<Page1 />} />
                   <Route path="page1_1" element={<Page1_1 />} />
                   <Route path="page2_1" element={<Page2_1 />} />
