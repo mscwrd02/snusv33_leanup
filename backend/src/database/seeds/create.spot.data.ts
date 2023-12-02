2e6ec05bf72e50c817fc860aeb655eb26f3cdf0e
okj <okj001010@gmail.com>
2023-12-03 03:30:40 +0900

    FIX: csv seed final

--- backend/src/database/seeds/create.spot.data.ts
+++ backend/src/database/seeds/create.spot.data.ts
@@ -1,47 +1,39 @@
 import { Seeder } from 'typeorm-extension';
 import { DataSource } from 'typeorm';
 import { Spots } from '../../entities/Spots';
-import { Region } from '../../entities/common/Region';
-export default class SpotSeeder implements Seeder {
+import { parse } from 'csv-parse';
+import * as fs from 'fs';
+
+export default class CSVSpotSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
     const spotsRepository = dataSource.getRepository(Spots);
-
-    await spotsRepository.insert([
-      {
-        name: '새별오름',
-        address: '제주특별자치도 제주시 애월읍 봉성리 산 59-8',
-        overview: '광진이 화이팅!!',
-        reviews: 10,
-        region: Region.EAST,
-      },
-      {
-        name: '쇠소깍',
-        address: '제주특별자치도 서귀포시 쇠소깍로 104',
-        overview: '광진이가 산책하기 좋은 곳',
-        reviews: 20,
-        region: Region.EAST,
-      },
-      {
-        name: '헬로키티아일랜드',
-        address: '제주특별자치도 서귀포시 안덕면 한창로 340',
-        overview: '광진이 헬로키티 귀요미 >_<',
-        reviews: 30,
-        region: Region.EAST,
-      },
-      {
-        name: '오설록 티뮤지엄',
-        address: '제주특별자치도 서귀포시 안덕면 신화역사로 15',
-        overview: '광진이가 마시는 티는 캐모마일',
-        reviews: 40,
-        region: Region.EAST,
-      },
-      {
-        name: '금능해수욕장',
-        address: '제주특별자치도 제주시 한림읍 금능길 119-10',
-        overview: '광진이 수영 못 함 ㅋㅋㅋ',
-        reviews: 50,
-        region: Region.EAST,
-      },
-    ]);
+    const spots: Spots[] = [];
+    const parseOptions = {
+      columns: true,
+    };
+    fs.createReadStream('src/database/seeds/spots.csv')
+      .pipe(parse(parseOptions))
+      .on('data', (row) => {
+        const spot = new Spots();
+        spot.name = row.name;
+        spot.address = row.address;
+        spot.hours = row.hours;
+        spot.fee = row.fee;
+        spot.takenTime = row.taken_time;
+        spot.overview = row.overview;
+        spot.feature1 = row.feature1;
+        spot.feature2 = row.feature2;
+        spot.feature3 = row.feature3;
+        spot.reviews = parseInt(row.reviews);
+        spot.region = row.region;
+        spot.link = row.link;
+        spots.push(spot);
+      })
+      .on('end', async () => {
+        await spotsRepository.save(spots);
+        console.log(
+          'CSV file successfully processed and saved in spotsRepository',
+        );
+      });
   }
 }
