1bc9beffe57e29a01762fd56a50ff865446212e0
mscwrd02 <ehdtodvodl@gmail.com>
2023-12-04 12:53:51 +0900

    seed file run order fixed

--- backend/src/database/seeds/create.spot.image.ts
+++ backend/src/database/seeds/create.spot.image.ts
@@ -4,14 +4,42 @@ import { parse } from 'csv-parse';
 import * as fs from 'fs';
 import { SpotImages } from '../../entities/SpotImages';
 import { SpotCategories } from '../../entities/SpotCategories';
+import { Spots } from '../../entities/Spots';
 
 export default class CSVSpotImageSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
-    const spotImagesRepository = dataSource.getRepository(SpotImages);
-    const spotCategoryRepository = dataSource.getRepository(SpotCategories);
+    const spotsRepository = dataSource.getRepository(Spots);
+    const spots: Spots[] = [];
     const parseOptions = {
       columns: true,
     };
+    await fs
+      .createReadStream(process.env.CSV_PATH + '/spots.csv')
+      .pipe(parse(parseOptions))
+      .on('data', (row) => {
+        const spot = new Spots();
+        spot.name = row.name;
+        spot.address = row.address;
+        spot.hours = row.hours;
+        spot.fee = row.fee;
+        spot.takenTime = row.taken_time;
+        spot.overview = row.overview;
+        spot.feature1 = row.feature1;
+        spot.feature2 = row.feature2;
+        spot.feature3 = row.feature3;
+        spot.reviews = parseInt(row.reviews);
+        spot.region = row.region;
+        spot.link = row.link;
+        spots.push(spot);
+      })
+      .on('end', async () => {
+        await spotsRepository.save(spots);
+        console.log(
+          'CSV file successfully processed and saved in spotsRepository',
+        );
+      });
+    const spotImagesRepository = dataSource.getRepository(SpotImages);
+    const spotCategoryRepository = dataSource.getRepository(SpotCategories);
     const spotImages: SpotImages[] = [];
     fs.createReadStream(process.env.CSV_PATH + '/spot_images.csv')
       .pipe(parse(parseOptions))
